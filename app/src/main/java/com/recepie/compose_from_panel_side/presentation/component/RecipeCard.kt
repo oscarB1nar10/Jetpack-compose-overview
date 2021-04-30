8f2d814766795dd68edff7de02b42f51d7918d19
Oscar <oscivaramtin@gmail.com>
2021-04-29 21:57:32 -0500

    AS-6
    AS-6: Missing files from the previous commit included.

--- app/src/main/java/com/recepie/compose_from_panel_side/presentation/component/RecipeCard.kt
+++ app/src/main/java/com/recepie/compose_from_panel_side/presentation/component/RecipeCard.kt
@@ -1,4 +1,85 @@
 package com.recepie.compose_from_panel_side.presentation.component
 
-class RecipeCard {
+import DEFAULT_RECIPE_IMAGE
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.recepie.compose_from_panel_side.domain.model.Recipe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import loadPicture
+
+@ExperimentalCoroutinesApi
+@Composable
+fun RecipeCard(
+    recipe: Recipe,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .padding(
+                bottom = 6.dp,
+                top = 6.dp,
+                start = 16.dp,
+                end = 16.dp
+            ),
+        shape = MaterialTheme.shapes.medium,
+        elevation = 8.dp
+    ) {
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+        ) {
+            recipe.featuredImage?.let { url ->
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE)
+                image.value?.let { bitmap ->
+                    Image(
+                        bitmap = bitmap.asImageBitmap(),
+                        contentDescription = "Plate image",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(align = Alignment.CenterVertically),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+
+            recipe.title?.let { title ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp)
+                ) {
+                    Text(
+                        text = title,
+                        modifier = Modifier
+                            .fillMaxWidth(0.85f)
+                            .wrapContentWidth(Alignment.Start),
+                        style = MaterialTheme.typography.h5,
+                    )
+
+                    Text(
+                        text = recipe.rating.toString(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.End)
+                            .wrapContentHeight(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.h6
+                    )
+
+                }
+            }
+        }
+
+
+    }
+
 }
