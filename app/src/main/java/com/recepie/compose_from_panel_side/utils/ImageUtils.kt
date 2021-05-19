92d2a678b4608baa06df166fdbc309c54313226b
Oscar <oscivaramtin@gmail.com>
2021-05-18 21:34:59 -0500

    AS-10
    AS-10 (Heart animation): Animation for a image that contains a heart image resource was included.

--- app/src/main/java/com/recepie/compose_from_panel_side/utils/ImageUtils.kt
+++ app/src/main/java/com/recepie/compose_from_panel_side/utils/ImageUtils.kt
@@ -51,3 +51,27 @@ fun loadPicture(url: String, @DrawableRes defaultImage: Int): MutableState<Bitma
 
     return bitmapState
 }
+
+@SuppressLint("UnrememberedMutableState")
+@ExperimentalCoroutinesApi
+@Composable
+fun loadPicture(@DrawableRes drawable: Int): MutableState<Bitmap?> {
+
+    val bitmapState: MutableState<Bitmap?> = mutableStateOf(null)
+
+    // show default image while image loads
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(drawable)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onLoadCleared(placeholder: Drawable?) {}
+            override fun onResourceReady(
+                resource: Bitmap,
+                transition: Transition<in Bitmap>?
+            ) {
+                bitmapState.value = resource
+            }
+        })
+
+    return bitmapState
+}
