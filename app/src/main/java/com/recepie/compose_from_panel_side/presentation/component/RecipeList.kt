3c446487f798aa4655bf4d49441f6ed6802af594
Oscar <oscivaramtin@gmail.com>
2021-07-13 21:56:53 -0500

    AS-21
    AS-21: State hosting patter included on [RecipeListFragment] to separate the recipe list logic to a decoupled and state less version.

---
+++ app/src/main/java/com/recepie/compose_from_panel_side/presentation/component/RecipeList.kt
@@ -0,0 +1,93 @@
+package com.recepie.compose_from_panel_side.presentation.component
+
+import android.os.Bundle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.recepie.compose_from_panel_side.R
+import com.recepie.compose_from_panel_side.domain.model.Recipe
+import com.recepie.compose_from_panel_side.presentation.component.util.SnackBarController
+import com.recepie.compose_from_panel_side.presentation.ui.recipe_list.PAGE_SIZE
+import com.recepie.compose_from_panel_side.presentation.ui.recipe_list.RecipeListEvent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+
+
+@ExperimentalMaterialApi
+@ExperimentalCoroutinesApi
+@Composable
+fun RecipeList(
+    loading: Boolean,
+    recipes: List<Recipe>,
+    onChangeRecipeScrollPosition: (Int) -> Unit,
+    page: Int,
+    onNextPage: (RecipeListEvent) -> Unit,
+    scaffoldState: ScaffoldState,
+    snackBarController: SnackBarController,
+    navController: NavController
+) {
+
+    Box(
+        modifier = Modifier
+            .background(color = MaterialTheme.colors.surface)
+    ) {
+        if (loading && recipes.isEmpty()) {
+            ShimmerRecipeCardItem(
+                imageHeight = 250.dp,
+                padding = 8.dp
+            )
+        } else {
+            LazyColumn {
+                itemsIndexed(
+                    items = recipes
+                ) { index, recipe ->
+                    onChangeRecipeScrollPosition(index)
+
+                    if ((index + 1) >= (page * PAGE_SIZE) && !loading) {
+                        onNextPage(RecipeListEvent.NextPageEvent)
+                    }
+
+                    RecipeCard(
+                        recipe = recipe,
+                        onClick = {
+                            if (recipe.id != null) {
+                                val bundle = Bundle()
+                                bundle.putInt("recipeId", recipe.id)
+
+                                navController.navigate(
+                                    R.id.action_recipeListFragment_to_recipeFragment,
+                                    bundle
+                                )
+
+                            } else {
+                                snackBarController.getScope().launch {
+                                    snackBarController.showSnackbar(
+                                        scaffoldState = scaffoldState,
+                                        message = "Recipe error",
+                                        actionLabel = "Ok"
+                                    )
+                                }
+                            }
+                        })
+                }
+            }
+        }
+        CircularIndeterminateProgressBar(isDisplayed = loading)
+        DefaultSnackbar(
+            snackbarHostState = scaffoldState.snackbarHostState,
+            onDismiss = {
+                scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+            },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
